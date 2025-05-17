7de9a24956b5057aa0e14eeb2177e785d0944d3c
mayankadhikari <87277420+mayankadhikari@users.noreply.github.com>
2025-05-17 23:35:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Replace all 0's with 5/replace-all-0s-with-5.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+/*you are required to complete this method*/
+class Solution {
+  public:
+    int convertFive(int n) {
+        // Your code here
+        string temp=to_string(n)+ "";
+        replace(temp.begin(),temp.end(),'0','5');
+        return stoi(temp);
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.convertFive(n) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+}
+// } Driver Code Ends
