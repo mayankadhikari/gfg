bc1643cf3a62a54236b37da388d30a0078ddd242
mayankadhikari <87277420+mayankadhikari@users.noreply.github.com>
2025-05-22 23:01:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Deletions/minimum-deletions.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int longestPalinSubseq(string &s){
+        int n=s.size();
+        vector<int> curr(n),prev(n);
+        for(int i=n-1;i>=0;--i){
+            curr[i]=1;
+            for(int j=i+1;j<n;++j){
+                if(s[i]==s[j]) curr[j]=prev[j-1]+2;
+                else curr[j]=max(prev[j],curr[j-1]);
+            }
+            prev=curr;
+        }
+        return curr[n-1];
+    }
+    int minDeletions(string s) {
+        // code here
+        int n=s.length();
+        int lps=longestPalinSubseq(s);
+        return n-lps;
+    }
+};
