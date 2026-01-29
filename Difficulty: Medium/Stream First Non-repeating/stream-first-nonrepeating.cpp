e4a71ed9cb0701245f06decb3749758eb984916e
mayankadhikari <87277420+mayankadhikari@users.noreply.github.com>
2026-01-29 12:42:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stream First Non-repeating/stream-first-nonrepeating.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    string firstNonRepeating(string &s) {
+        // code here
+        int n=s.length();
+        queue<char> q;
+        unordered_map<char,int> mp;
+        string res="";
+        for(int i=0;i<n;i++){
+            if(++mp[s[i]]==1) q.push(s[i]);
+            while(!q.empty() && mp[q.front()]>1) q.pop();
+            res+=q.empty()?'#':q.front();
+        }
+        return res;
+    }
+};
